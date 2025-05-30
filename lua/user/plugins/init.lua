0a3ee101ddfab41e16fd52f45a69b1cbb849da42
Le_SaRo <24613248+LeSaRo@users.noreply.github.com>
2025-05-30 16:57:52 +0200

    Cleanup, Added colorizer

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -2,4 +2,5 @@ return {
     { "lewis6991/gitsigns.nvim", opts = {}, },
     { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons"}, opts = {}, },
     { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = {}, },
+    { "norcalli/nvim-colorizer.lua" },
 }
